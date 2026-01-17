86c97074c157958ddbb7b1e150f226e3422ef29b
melinajonesb <s253737@dtu.dk>
2026-01-17 14:07:16 +0100

    edited test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from src.clickbait_classifier.api import app
 import pytest
+from unittest.mock import patch
 
 client = TestClient(app)
 
@@ -11,10 +12,17 @@ def test_read_main():
     # Tilpass denne meldingen til det API-et ditt faktisk returnerer
     assert response.json() == {"message": "OK", "status-code": 200}
 
-def test_predict_endpoint():
+
+@patch("src.clickbait_classifier.api.tokenizer") # Patcher tokenizer i api.py
+@patch("src.clickbait_classifier.api.model")
+
+def test_predict_endpoint(mock_model, mock_tokenizer):
+    """Denne tester API-logikken uten å laste tunge filer."""
+    # Vi simulerer hva modellen skal returnere
+    mock_model.return_value = "mocked_prediction" 
+    
     with TestClient(app) as client:
         response = client.post("/predict?text=Dette er en test")
         assert response.status_code == 200
-        json_data = response.json()
-        assert "is_clickbait" in json_data
-        assert json_data["text"] == "Dette er en test"
+        # Vi sjekker at API-et sender ut de riktige nøklene
+        assert "is_clickbait" in response.json()
